05e412153749637445144431c5ee3ff387babef1
meh <4267552+nn-prog@users.noreply.github.com>
2021-12-03 20:57:05 +0000

    loop for reverb bit crusher piano

--- reverb_bit_crusher_piano.rb
+++ reverb_bit_crusher_piano.rb
@@ -1,24 +1,19 @@
 # Welcome to Sonic Pi
 
-use_synth :piano
-
-chords = []
-i = 0
-
-
-2.times do
+loop do
+  use_synth :piano
   with_fx :reverb, room: 1 do
-    8.times do
-      with_fx :bitcrusher, cutoff: 80 do
-        tone  = chord(:E3, :m11).choose
-        play tone, amp: 0.4
-        chords << tone
+    with_fx :bitcrusher, cutoff: 80 do
+      2.times do
+        8.times do
+          tone  = chord(:E3, :m11).choose
+          play tone, amp: 0.4
+          sleep [0.21,0.23].choose
+        end
+        sleep 3
       end
-      sleep [0.21,0.23].choose
     end
-    sleep 3
   end
+  clear
 end
-puts chord(:E3, :m11)
-puts chords
 
